ef570d497aac528a94d16881559f1c21323d9a17
jack <jackchanxj@163.com>
2014-11-09 10:09:05 +0800

    修改数据库驱动

--- upload2/config.php
+++ upload2/config.php
@@ -18,7 +18,7 @@ define('DIR_DOWNLOAD', '/var/www/html/qingtian/upload2/download/');
 define('DIR_LOGS', '/var/www/html/qingtian/upload2/system/logs/');
 
 // DB
-define('DB_DRIVER', 'mysql');
+define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
